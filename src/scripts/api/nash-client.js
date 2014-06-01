7703d6cacdb9506c0d50872bb24f3eca9775d84a
Adam Reeves <dark.account@gmail.com>
2014-05-31 21:35:44 -0500

    Add getParkData method;

--- src/scripts/api/nash-client.js
+++ src/scripts/api/nash-client.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 var Soda = require('./soda-client');
 
 function NashClient(appToken, username, password) {
@@ -14,6 +15,38 @@ NashClient.prototype = Object.create(Soda.prototype);
 
 NashClient.prototype.getParkLocations = function (params, callback) {
 	this.get('74d7-b74t', params, callback);
+};
+
+NashClient.prototype.getParkSpecialEventPermits = function (params, callback) {
+	this.get('vygj-v677', params, callback);
+};
+
+NashClient.prototype.getParkData = function (params, callback) {
+	var events, locations, error,
+		done = false;
+	function tryComplete() {
+		if (done) {
+			return;
+		}
+		if (error) {
+			done = true;
+			callback(error);
+		}
+		if (events && locations) {
+			done = true;
+			callback(null, locations.concat(events));
+		}
+	}
+	this.getParkLocations(params, function (err, data) {
+		error = err || error;
+		locations = data;
+		tryComplete();
+	});
+	this.getParkSpecialEventPermits(params, function (err, data) {
+		error = err || error;
+		events = data;
+		tryComplete();
+	});
 };
 
 module.exports = NashClient;
